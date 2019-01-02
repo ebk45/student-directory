f8b78a0830a61f6ae1383bc236e480abcf9db530
ellie turnock <ebturnock@gmail.com>
2019-01-02 10:22:21 +0000

    refactor student list

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 #first print the list of students
 puts "The students of Villains Academy"
 puts "------------"
@@ -13,7 +14,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 # print total no. of students
-print "Overall, we have "
-#using print and puts to change paragraph spacing
-print 11
-puts " great students"
+puts "Overall, we have #{student_count} great students"
