0952991a9d24bff7f9fce6f1425a938cda3bf68e
ellie turnock <ebturnock@gmail.com>
2019-01-02 19:16:57 +0000

    method added - search by student initial

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students: "
   puts "To finish, just hit enter/return twice"
   students = []
   name = gets.chomp
@@ -8,6 +8,7 @@ def input_students
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
+  #this returns the array of the students the user has inputted
   students
 end
 
@@ -16,10 +17,23 @@ def print_header
   puts "------------"
 end
 
-def print(students)
-  students.each_with_index do | (student, cohort), index|
+def print(names)
+  names.each_with_index do | student, index |
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
+end
+
+def print_by_initial(students)
+  puts "You can search the directory by a students first initial."
+  puts "Please enter an initial: "
+  initial_input = gets.chomp
+  count = 1
+  students.each {|student|
+    if student[:name][0] == initial_input.upcase
+      puts "#{count}: #{student[:name]} (#{student[:cohort]} cohort)"
+      count += 1
+    end
+  }
 end
 
 def print_footer(students)
@@ -27,6 +41,7 @@ def print_footer(students)
 end
 
 students = input_students
+#returns array of students that will be the argument in the following methods
 print_header
-print(students)
+print_by_initial(students)
 print_footer(students)
