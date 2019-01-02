1dc7cce4e59d215653a34a6511e99fe54ab8dbe4
ellie turnock <ebturnock@gmail.com>
2019-01-02 14:18:44 +0000

    add accummulator to number student list

--- directory.rb
+++ directory.rb
@@ -1,31 +1,3 @@
-#put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name:" Norman Bates", cohort: :november}
-]
-# print student list
-def print_header
-  puts "The students of Villains Academy"
-  puts "------------"
-end
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-#getting user input
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit enter/return twice"
@@ -38,7 +10,24 @@ def input_students
   end
   students
 end
-#call newly defined methods
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "------------"
+end
+
+def print(students)
+  count = 1
+  students.each do |student|
+    puts "#{count}. #{student[:name]} (#{student[:cohort]} cohort)"
+    count += 1
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
 students = input_students
 print_header
 print(students)
