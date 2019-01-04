457fff027616bc7ddc3bf87ab3415698d8f6cb7c
ellie turnock <ebturnock@gmail.com>
2019-01-04 12:04:23 +0000

    added .center alignment for output

--- directory.rb
+++ directory.rb
@@ -1,46 +1,47 @@
 def input_students
-  puts "Please enter the names of the students: "
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students:".center(80)
+  puts "To finish, just hit return twice".center(80)
   students = []
-  name = gets.chomp.capitalize
+  name = gets.chomp.upcase
   while !name.empty? do
-      puts "Please enter the student's cohort: "
+      puts "Please enter the student's cohort:".center(80)
       cohort = gets.chomp.capitalize
-      puts "Please enter the student's age: "
+      puts "Please enter the student's age:".center(80)
       age = gets.chomp
-      puts "Please enter the student's country of birth: "
+      puts "Please enter the student's country of birth:".center(80)
       country = gets.chomp.capitalize
       students << {name: name, cohort: cohort,
                    age: age, country: country}
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{students.count} students".center(80)
       #we ask the user for another name input because name
       #is currently defined outside the loop
-      name = gets.chomp.capitalize
+      name = gets.chomp.upcase
   end
   #this returns the array of student info hashes the user has inputted
   students
 end
 
 def print_header
-  puts "The students of Villains Academy", "------------"
+  puts "The students of Villains Academy:".center(80)
+  puts "---------------------------------".center(80)
 end
 
 def print(students)
   count = 0
   while count < students.count
-    puts "#{count + 1}. #{students[count][:name]}, #{students[count][:age]} (#{students[count][:cohort]} cohort) - #{students[count][:country]}"
+    puts "#{count + 1}. #{students[count][:name]}, #{students[count][:age]} (#{students[count][:cohort]} Cohort) - #{students[count][:country]}".center(80)
     count += 1
   end
 end
 
 def print_by_initial(students)
-  puts "You can search the directory by a students first initial."
-  puts "Please enter an initial: "
+  puts "You can search the directory by a students first initial.".center(80)
+  puts "Please enter an initial:".center(80)
   initial_input = gets.chomp
   count = 1
   students.each {|student|
     if student[:name][0] == initial_input.upcase
-      puts "#{count + 1}. #{students[count][:name]}, #{students[count][:age]} (#{students[count][:cohort]}) - #{students[count][:country]}"
+      puts "#{count + 1}. #{students[count][:name]}, #{students[count][:age]} (#{students[count][:cohort]} Cohort) - #{students[count][:country]}".center(80)
       count += 1
     end
   }
@@ -50,15 +51,15 @@ def print_under_12_chars(students)
   count = 1
   students.each {|student|
     if student[:name].length < 12
-      puts "#{count + 1}. #{students[count][:name]}, #{students[count][:age]} (#{students[count][:cohort]}) - #{students[count][:country]}"
+      puts "#{count + 1}. #{students[count][:name]}, #{students[count][:age]} (#{students[count][:cohort]} Cohort) - #{students[count][:country]}".center(80)
       count += 1
     end
   }
 end
 
 def print_footer(students)
-  puts "There are #{students.count} students with names under 12 characters."
-  puts "This includes punctuation and spacing."
+  puts "There are #{students.count} students with names under 12 characters.".center(80)
+  puts "This includes punctuation and spacing.".center(80)
 end
 
 students = input_students
