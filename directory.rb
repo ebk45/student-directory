aa7f659b545a5762498e6d16992cc906e5ea7f47
ellie turnock <ebturnock@gmail.com>
2019-01-04 19:06:33 +0000

    added interactive menu for directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,29 @@
+def interactive_menu
+  students = []
+  loop do
+  # 1. print menu and ask user what they want to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" #because we're adding more items later
+
+    selection = gets.chomp
+
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "3"
+      exit #this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+
 def input_students
   puts "Please enter the names of the students:".center(80)
   puts "To finish, just hit return twice".center(80)
@@ -44,6 +70,7 @@ def print(students)
   while count < students.count
     puts "#{count + 1}. #{students[count][:name]}, #{students[count][:age]} (#{students[count][:cohort]} Cohort) - #{students[count][:country]}".center(80)
     count += 1
+    break if students.empty?
   end
 end
 
@@ -68,6 +95,4 @@ def print_footer(students)
   puts "This includes punctuation and spacing.".center(80)
 end
 
-students = input_students
-#returns array of students that will be the argument in the following methods
-print_by_cohort(students)
+interactive_menu
