70d9299e7f19a31260876a12336f5e8ff6698143
ellie turnock <ebturnock@gmail.com>
2019-01-05 20:05:50 +0000

    fixed error in cohort grouping and refactored

--- directory.rb
+++ directory.rb
@@ -1,5 +1,7 @@
 @students = [] #global array for student data
 
+require 'csv'
+
 def interactive_menu
   loop do
     print_menu
@@ -9,9 +11,9 @@ end
 
 def print_menu
   puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "2. View a student list"
+  puts "3. Save student list"
+  puts "4. Load a student list"
   puts "0. Exit"
 end
 
@@ -34,10 +36,21 @@ def menu_actions(selection)
 end
 
 def show_students
-  puts
-  print_header
-  prints_students_list(@students)
-  print_footer(@students)
+  puts "How would you like to view?"
+  puts "1. As they were inputted"
+  puts "2. By cohort group"
+  view_input = gets.chomp
+  if view_input == "1"
+    print_header
+    prints_students_list
+    print_footer
+  elsif view_input == "2"
+    print_header
+    prints_by_cohort
+    print_footer
+  else
+    puts "That isn't a valid selection, please try again."
+  end
 end
 
 def input_students
@@ -76,24 +89,24 @@ def print_header
   puts "---------------------------------".center(80)
 end
 
-def prints_students_list(students)
+def prints_students_list
   count = 0
-  while count < students.count
-    puts "#{count + 1}. #{students[count][:name]}, #{students[count][:age]} (#{students[count][:cohort]} Cohort) - #{students[count][:country]}".center(80)
+  while count < @students.count
+    puts "#{count + 1}. #{@students[count][:name]}, #{@students[count][:age]} (#{@students[count][:cohort]} Cohort) - #{@students[count][:country]}".center(80)
     count += 1
-    break if students.empty?
+    break if @students.empty?
   end
 end
 
-def prints_by_cohort(students)
+def prints_by_cohort
   cohort_array = []
-  students.map { |student| cohort_array << student[:cohort] }
+  @students.map { |student| cohort_array << student[:cohort] }
   newcohort_array = cohort_array.uniq
   count = 0
   while count < newcohort_array.length do
-    puts "#{cohort_array[count]} Cohort: ".center(80)
-    students.each do |student|
-      if student[:cohort] == cohort_array[count]
+    puts "#{newcohort_array[count]} Cohort: ".center(80)
+    @students.each do |student|
+      if student[:cohort] == newcohort_array[count]
         puts "#{student[:name]}, #{student[:age]} - #{student[:country]}".center(80)
       end
     end
@@ -101,11 +114,11 @@ def prints_by_cohort(students)
   end
 end
 
-def print_footer(students)
-  if students.count == 1
-    puts "Overall, we have #{students.count} great student.".center(80)
-  else students.count > 1
-    puts "Overall, we have #{students.count} great students.".center(80)
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student.".center(80)
+  else @students.count > 1
+    puts "Overall, we have #{@students.count} great students.".center(80)
   end
 end
 
@@ -114,19 +127,18 @@ def input_student_array(name, cohort, age, country)
 end
 
 def save_students
-  puts "What would you like to name your file?"
+  puts "What would you like to name your file? Will save to 'students.csv' by default."
   filename = STDIN.gets.chop
   filename = "students.csv" if filename.nil?
-  File.open("#{filename}", "w") do |file|
+  CSV.open(filename, "w") do |csv|
     @students.each do |student|
-    student_data = [student[:name], student[:cohort], [student[:age]], [student[:country]]].join(",")
-    file.puts student_data
-   end
+    csv << [student[:name], student[:cohort], [student[:age]], [student[:country]]]
+    end
   end
   puts "Student List saved to #{filename}"
 end
 
-def load_csv_on_startup
+def load_on_startup
   filename = ARGV.first #first argument from the command line
   filename = "students.csv" if filename.nil?
   if File.exists?(filename)
@@ -139,15 +151,12 @@ def load_csv_on_startup
 end
 
 def load_students_from_file(filename = "students.csv")
-  file = File.open("#{filename}", "r")
-  file.readlines.each do |line|
-    name, cohort, age, country = line.chomp.split(',')
-    input_student_array(name, cohort, age, country)
+  CSV.foreach(filename) do |line|
+    input_student_array(line[0], line[1], line[2], line[3])
   end
-  file.close
   puts "The #{filename} file has been loaded successfully."
   puts "To view, select option 2."
 end
 
-load_csv_on_startup
+load_on_startup
 interactive_menu
